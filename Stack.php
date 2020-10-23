b432b13584652b90ab7593fedf700f085fb1c339
ducc1998 <duccoc98@gmail.com>
2020-10-23 14:33:08 +0700

    Reverse_array

---
+++ Stack.php
@@ -0,0 +1,33 @@
+<?php
+
+
+class Stack
+{
+    protected $stack;
+
+    public function __construct()
+    {
+        $this->stack = [];
+    }
+
+    public function push($element)
+    {
+        array_push($this->stack, $element);
+    }
+
+    public function pop()
+    {
+        if ($this->isEmpty()) {
+            echo "Ngan xep trong";
+        } else {
+            return array_pop($this->stack);
+        }
+    }
+
+    public function isEmpty()
+    {
+        return empty($this->stack);
+    }
+
+
+}
